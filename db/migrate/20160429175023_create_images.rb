7041dde156755656b0a2d354b81e624c69af7fbe
jvollherbst <jasmine.cardoza@gmail.com>
2016-04-29 14:19:04 -0400

    created users and images tables. installed paperclip

---
+++ db/migrate/20160429175023_create_images.rb
@@ -0,0 +1,12 @@
+class CreateImages < ActiveRecord::Migration
+  def change
+    create_table :images do |t|
+      t.string :imgname
+      t.string :category
+      t.string :tag
+      t.string :description
+      t.string :imgurl
+      t.attachment :image 
+    end
+  end
+end
