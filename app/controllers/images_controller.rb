3800c3c638f7f1cdbce8967930b8b166aa73681c
jvollherbst <jasmine.cardoza@gmail.com>
2016-04-30 19:16:14 -0400

    can create and render images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,27 @@
 class ImagesController < ApplicationController
-  def users
+
+  def index
+    @images = Image.all
   end
+
+  def new
+    @image = Image.new
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def create
+    @image = Image.create(image_params)
+    @image.save
+    redirect_to @image
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:imgname, :category, :tag, :description, :imgurl)
+  end
+
 end
