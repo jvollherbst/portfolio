9842318e8b8f763ec1428a5dbc8dc8b59d3676b5
jvollherbst <jasmine.cardoza@gmail.com>
2016-05-02 08:18:32 -0400

    added social icons, edit and delete features to form

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -10,6 +10,16 @@ class ImagesController < ApplicationController
     @image = Image.new
   end
 
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  def update
+    @image = Image.find(params[:id])
+    @image.update(image_params)
+    redirect_to @image
+  end
+
   def show
     @image = Image.find(params[:id])
   end
@@ -17,6 +27,12 @@ class ImagesController < ApplicationController
   def create
     @image = Image.create(image_params)
     @image.save
+    redirect_to @image
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
     redirect_to @image
   end
 
