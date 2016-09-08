fb856989c5f270da1506a634d23db95aacd9bb32
jvollherbst <jasmine.cardoza@gmail.com>
2016-09-07 23:11:51 -0400

    added category sorting. fixed image description transition

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,12 +2,33 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-window.onload = function() {
-  window.addEventListener('scroll', function(){
-    if(document.body.scrollTop > 150) {
-      document.getElementById("descr").classList.add('new-position');
-    } else if(document.body.scrollTop < 150) {
-      document.getElementById("descr").classList.remove('new-position');
+// window.onload = function() {
+//   window.addEventListener('scroll', function(){
+//     if(document.body.scrollTop > 150) {
+//       document.getElementById("descr").classList.add('new-position');
+//     } else if(document.body.scrollTop < 150) {
+//       document.getElementById("descr").classList.remove('new-position');
+//     }
+//   });
+// };
+
+$(document).ready(function() {
+
+  $(window).on('scroll', function(){
+    if($(window).scrollTop() > 150) {
+      $("#descr").addClass('new-position');
+    } else if($(window).scrollTop() < 150) {
+      $("#descr").removeClass('new-position');
     }
   });
-};
+
+  $('select').change(function(){
+    var catSelection = "";
+    $('select option:selected').each(function() {
+      catSelection = $(this).text();
+    });
+    $('.thumbnail').hide();
+    $('.' + catSelection).show();
+  });
+
+});
