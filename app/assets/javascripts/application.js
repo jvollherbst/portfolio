19082694afe77a13db901ed1655583dffeb0bb93
jvollherbst <jasmine.cardoza@gmail.com>
2017-02-20 14:57:36 -0500

    updated layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,26 +13,6 @@
 // };
 
 $(document).ready(function() {
-  var windowSize;
-  function responsive() {
-    windowSize = { width: window.innerWidth };
-    $(window).resize(responsive);
-    return windowSize;
-  };
-  responsive();
-
-  $(window).on('scroll', function(){
-    if(windowSize.width > 767) {
-      if($(window).scrollTop() > 90) {
-        $("#descr").addClass('new-position');
-      } else if($(window).scrollTop() < 90) {
-        $("#descr").removeClass('new-position');
-      }
-    } else if(windowSize.width < 767) {
-        $("#descr").removeClass('new-position');
-    }
-  });
-
 
   $(document).on('click', '.tags', function(){
     var catSelection = $(this).text();
@@ -43,18 +23,5 @@ $(document).ready(function() {
       $('.' + catSelection).show();
     }
   });
-
-  // $('select').change(function(){
-  //   var catSelection = "";
-  //   $('select option:selected').each(function() {
-  //     catSelection = $(this).text();
-  //   });
-  //   if(catSelection == "All") {
-  //     $('.thumbnail').show();
-  //   } else {
-  //     $('.thumbnail').hide();
-  //     $('.' + catSelection).show();
-  //   }
-  // });
 
 });
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502200010) do
+ActiveRecord::Schema.define(version: 20170220190446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
